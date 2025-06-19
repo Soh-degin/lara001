6b73354a806198a8820fcb25e8af8265ca6801bd
LAPTOP-5NOKQRU8\Rise0043 <teshigawara0000sou@gmail.com>
2025-06-19 20:33:47 +0900

    first commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    //
+    public function showList()
+    {
+      return  view('blog.list');
+    }
+}
